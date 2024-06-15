1537ac750c46d3bfcb7a05e0877cefb1fb679391
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-06-15 18:19:28 +0530

    ✅Update links in navbar and bottom banner to use root-relative URLs

--- public/index.php
+++ public/index.php
@@ -1,5 +1,13 @@
 <?php
 
 require "../helpers.php";
+require base_path('Router.php');
 
-loadView('home');
+$router = new Router();
+
+$routes = require base_path('routes.php');
+
+$uri = trim($_SERVER['REQUEST_URI']);
+$method = $_SERVER['REQUEST_METHOD'];
+
+$router->route($uri, $method, $routes);
